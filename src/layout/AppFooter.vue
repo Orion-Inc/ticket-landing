0c03430d8c026c27c4c1f569a7655da30fe2d93c
Farid Adam <faridibin@gmail.com>
2018-12-19 14:21:49 +0000

    Initial commit

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,36 @@
+<template>
+    <footer class="footer has-cards">
+        <div class="container">
+            <div class="row align-items-center justify-content-md-between">
+                <div class="col-md-6">
+                    <div class="copyright">
+                        &copy; 2018
+                        <a href="#">App Name</a>.
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <ul class="nav nav-footer justify-content-end">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">FAQs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {};
+</script>
+<style>
+</style>
